cf5f12358e520025a33e878a39308fa14d9c0322
SuKe <118174926+CloudMeiyu@users.noreply.github.com>
2025-01-21 14:46:44 +0800

    Update core_hook.c

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -164,9 +164,9 @@ void escape_to_root(void)
 	
 	// Refer to kernel/seccomp.c: seccomp_set_mode_strict
 	// When disabling Seccomp, ensure that current->sighand->siglock is held during the operation.
-	spin_lock_irq(&current->sighand->siglock);
+	// spin_lock_irq(&current->sighand->siglock);
 	// disable_seccomp();
-	spin_unlock_irq(&current->sighand->siglock);
+	// spin_unlock_irq(&current->sighand->siglock);
 
 	setup_selinux(profile->selinux_domain);
 }
