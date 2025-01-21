78b3f31da4d2bea9e8e6ea2c85e838bb46f62cc1
SuKe <118174926+CloudMeiyu@users.noreply.github.com>
2025-01-21 13:24:17 +0800

    Update core_hook.c
    try fix shell su

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -45,9 +45,7 @@
 #include "throne_tracker.h"
 #include "kernel_compat.h"
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0) || defined(KSU_COMPAT_GET_CRED_RCU)
-#define KSU_GET_CRED_RCU
-#endif
+
 
 static bool ksu_module_mounted = false;
 
@@ -114,42 +112,11 @@ static void setup_groups(struct root_profile *profile, struct cred *cred)
 	set_groups(cred, group_info);
 }
 
-static void disable_seccomp(void)
-{
-	assert_spin_locked(&current->sighand->siglock);
-	// disable seccomp
-#if defined(CONFIG_GENERIC_ENTRY) &&                                           \
-	LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)
-	current_thread_info()->syscall_work &= ~SYSCALL_WORK_SECCOMP;
-#else
-	current_thread_info()->flags &= ~(TIF_SECCOMP | _TIF_SECCOMP);
-#endif
-
-#ifdef CONFIG_SECCOMP
-	current->seccomp.mode = 0;
-	current->seccomp.filter = NULL;
-#else
-#endif
-}
 
 void escape_to_root(void)
 {
 	struct cred *cred;
-
-#ifdef KSU_GET_CRED_RCU
-	rcu_read_lock();
-
-	do {
-		cred = (struct cred *)__task_cred((current));
-		BUG_ON(!cred);
-	} while (!get_cred_rcu(cred));
-
-	if (cred->euid.val == 0) {
-		pr_warn("Already root, don't escape!\n");
-		rcu_read_unlock();
-		return;
-	}
-#else
+	
 	cred = (struct cred *)__task_cred(current);
 
 	if (cred->euid.val == 0) {
@@ -195,14 +162,10 @@ void escape_to_root(void)
 
 	setup_groups(profile, cred);
 	
-#ifdef KSU_GET_CRED_RCU
-	rcu_read_unlock();
-#endif
-
 	// Refer to kernel/seccomp.c: seccomp_set_mode_strict
 	// When disabling Seccomp, ensure that current->sighand->siglock is held during the operation.
 	spin_lock_irq(&current->sighand->siglock);
-	disable_seccomp();
+	// disable_seccomp();
 	spin_unlock_irq(&current->sighand->siglock);
 
 	setup_selinux(profile->selinux_domain);
